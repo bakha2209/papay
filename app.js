c4463655f6e93bc0a1fdf7e3505d485c8c1bed80
bakha2209 <bakhodir2209@gmail.com>
2023-10-21 09:49:01 +0900

    feat: develop session authentication on ejs

--- app.js
+++ app.js
@@ -4,13 +4,35 @@ const app = express();
 const router = require("./router");
 const router_bssr = require("./router_bssr.js")
 
+let session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URL,
+  collection: "sessions",
+})
+
+
 // 1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // 2 Session code
-
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 1000*60*30,
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(function(req, res, next) {
+  res.locals.member = req.session.member;
+  next();
+})
 
 // 3 Views code
 app.set("views", "views");
--- controllers/restaurantControllers.js
+++ controllers/restaurantControllers.js
@@ -2,6 +2,19 @@ const Member = require("../models/Member")
 
 let restaurantController = module.exports;
 
+restaurantController.getMyRestaurantData = async (req, res) => {
+  try{
+    console.log(`GET: cont/getMyRestaurantData`)
+    //TODO: Get my restaurant products
+
+    res.render('restaurant-menu');
+
+  } catch(err) {
+    console.log(`ERROR, cont/getMyRestaurantData, ${err.message}`)
+    res.json({state: 'fail', message: err.message})
+  }
+}
+
 restaurantController.getSignupMyRestaurant = async (req, res) => {
   try{
     console.log(`GET: cont/getSignupMyRestaurant`)
@@ -20,7 +33,9 @@ restaurantController.signupProcess = async (req,res) => {
       member = new Member(),
       new_member = await member.signupData(data);
 
-    res.json({state: 'succeed', data: new_member})
+    req.session.member = new_member;
+    res.redirect("/resto/products/menu");
+  
   } catch(err) {
     console.log(`ERROR, cont/signup, ${err.message}`)
     res.json({state: 'fail', message: err.message})
@@ -45,7 +60,11 @@ restaurantController.loginProcess = async (req,res) => {
       member = new Member(),
       result = await member.loginData(data);
 
-    res.json({state: 'succeed', data: result})
+    req.session.member = result;
+    req.session.save(function (){
+      res.redirect("/resto/products/menu");
+    });
+    
   } catch(err) {
     console.log(`ERROR, cont/login, ${err.message}`)
     res.json({state: 'fail', message: err.message})
@@ -56,3 +75,11 @@ restaurantController.logout = (req,res) => {
   console.log("GET cont.logout")
   res.send("logout sahifasidasiz");  
 }
+
+restaurantController.checkSessions = (req,res) => {
+  if (req.session.member) {
+    res.json({state: "succeed", data: req.session.member});
+  } else {
+    res.json({state: "fail", message: "You are not authenticated"});
+  } 
+};
