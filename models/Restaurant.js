5a1d336d8b8ce19e8f382b1c3a528409fab98af0
bakha2209 <bakhodir2209@gmail.com>
2023-11-09 13:13:45 +0900

    feat: develop update restaurant member top data by Admin

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,6 +2,7 @@ const assert = require("assert");
 
 const MemberModel = require("../schema/member_model");
 const Definer = require("../lib/mistake");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 
 class Restaurant {
   constructor() {
@@ -13,6 +14,24 @@ class Restaurant {
       const result = await this.memberModel
         .find({
           mb_type: "RESTAURANT",
+        })
+        .exec();
+
+      assert.ok(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateRestaurantByAdminData(update_data) {
+    try {
+      const id = shapeIntoMongooseObjectId(update_data?.id);
+      const result = await this.memberModel
+        .findByIdAndUpdate({ _id: id }, update_data, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after",
         })
         .exec();
 
