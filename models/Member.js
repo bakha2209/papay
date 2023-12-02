38f9a0a5e0ad4cd5d667b49fe9ff4f6b586b4c8d
bakha2209 <bakhodir2209@gmail.com>
2023-12-02 10:06:51 +0900

    feat: getChosenMember rest API development part one

--- models/Member.js
+++ models/Member.js
@@ -2,7 +2,7 @@ const MemberModel = require("../schema/member_model");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
 const bcrypt = require("bcryptjs");
-
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 
 class Member {
   constructor() {
@@ -18,25 +18,23 @@ class Member {
       let result;
       try {
         result = await new_member.save();
-      } catch(mongo_err) {
-        console.log(mongo_err)
-        throw new Error(Definer.auth_err1)
+      } catch (mongo_err) {
+        console.log(mongo_err);
+        throw new Error(Definer.auth_err1);
       }
-      
+
       result.mb_password = "";
       return result;
-    } catch(err) {
-      throw err
+    } catch (err) {
+      throw err;
     }
   }
 
   async loginData(input) {
     try {
       const member = await this.memberModel
-      .findOne(
-        {mb_nick: input.mb_nick},
-        {mb_nick: 1, mb_password:1}
-      ).exec();
+        .findOne({ mb_nick: input.mb_nick }, { mb_nick: 1, mb_password: 1 })
+        .exec();
 
       assert.ok(member, Definer.auth_err3);
       //console.log(member)
@@ -46,16 +44,30 @@ class Member {
       );
       assert.ok(isMatch, Definer.auth_err4);
 
-      return await this.memberModel.findOne({mb_nick: input.mb_nick}).exec();
-
-
-      
-    } catch(err) {
-      throw err
+      return await this.memberModel.findOne({ mb_nick: input.mb_nick }).exec();
+    } catch (err) {
+      throw err;
     }
   }
 
+  async getChosenMemberData(member, id) {
+    try {
+      id = shapeIntoMongooseObjectId(id);
 
+      console.log("member::", member)
+
+      const result = await this.memberModel
+        .aggregate([{
+          $match: { _id: id, mb_status: "ACTIVE" },
+        }, {$unset: "mb_password"}])
+        .exec();
+      
+      assert.ok(result, Definer.general_err2)
+      return result[0];  
+    } catch (err) {
+      throw err;
+    }
+  }
 }
 
 module.exports = Member;
--- router.js
+++ router.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const memberController =require("./controllers/memberControllers");
+const memberController = require("./controllers/memberControllers");
 
 /********************************
  *      REST API                *
-*********************************/
-
-
+ *********************************/
 
 //memberga dahldor routerlar
 
@@ -14,17 +12,19 @@ router.post("/signup", memberController.signup);
 router.post("/login", memberController.login);
 router.get("/logout", memberController.logout);
 router.get("/check-me", memberController.checkMyAuthentication);
-
-
+router.get(
+  "/member/:id",
+  memberController.retrieveAuthMember,
+  memberController.getChosenMember
+);
 
 //boshqa routerlar
-router.get("/menu", (req,res) => {
+router.get("/menu", (req, res) => {
   res.send("Menu sahifasidasiz");
 });
 
-router.get("/community", (req,res) => {
-  res.send("Jamiyat sahifasidasiz")
-})
+router.get("/community", (req, res) => {
+  res.send("Jamiyat sahifasidasiz");
+});
 
-
-module.exports = router
+module.exports = router;
