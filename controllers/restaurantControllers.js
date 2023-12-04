4b0d69bf7667dee9bd404f6f8390949ef4e56cf3
bakha2209 <bakhodir2209@gmail.com>
2023-12-04 22:05:54 +0900

    feat: develop getRestaurants api business logic

--- controllers/restaurantControllers.js
+++ controllers/restaurantControllers.js
@@ -6,6 +6,25 @@ const Restaurant = require("../models/Restaurant");
 
 let restaurantController = module.exports;
 
+restaurantController.getRestaurants = async (req, res) => {
+  try {
+    console.log("GET: cont/getRestaurants");
+    const data = req.query;
+    const restaurant = new Restaurant();
+    const result = await restaurant.getRestaurantsData(req.member, data)
+    //console.log("result:::", result)
+    res.json({state: "success", data: result})
+  } catch(err) {
+    console.log(`ERROR, cont/getRestaurants, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+}
+
+/****************************************
+ * BSSR RELATED METHODS
+ ****************************************/
+
+
 restaurantController.home = (req, res) => {
   try {
     console.log("GET: cont/home");
@@ -119,7 +138,7 @@ restaurantController.validateAuthRestaurant = (req, res, next) => {
 
 restaurantController.checkSessions = (req, res) => {
   if (req.session?.member) {
-    res.json({ state: "succeed", data: req.session.member });
+    res.json({ state: "success", data: req.session.member });
   } else {
     res.json({ state: "fail", message: "You are not authenticated" });
   }
