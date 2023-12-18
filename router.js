8dd93a61ea78ffe84ec35d077b232df78e163d5f
bakha2209 <bakhodir2209@gmail.com>
2023-12-18 21:34:59 +0900

    feat: develop getMemberFollowing rest api logic

--- router.js
+++ router.js
@@ -104,4 +104,8 @@ router.post(
   memberController.retrieveAuthMember,
   followController.unsubscribe
 );
+router.get(
+  "/follow/followings",
+  followController.getMemberFollowings
+);
 module.exports = router;
